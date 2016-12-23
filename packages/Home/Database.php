50ccb0aba9107070fa2c949a7b40b10b5d8a5b8d
ErkAydogan <erk.aydogan@ozu.edu.tr>
2016-12-23 23:48:33 +0200

    Update.

--- packages/Home/Database.php
+++ packages/Home/Database.php
@@ -1,19 +1,10 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: thena_000
- * Date: 12/22/2016
- * Time: 6:26 PM
- */
+
 class Database{
     public function __construct()
     {
         $this->conn = new mysqli("localhost", "root", "12345678", "havefun");
 
-        if ( $this->conn->connect_error) {
-            die("Connection failed: " . $this->conn->connect_error);
-
-        }
     }
 
     public function __destruct(){
