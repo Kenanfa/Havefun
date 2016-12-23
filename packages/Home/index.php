6b326e5cf12057e54da77cc78021bd666a76cc7c
ErkAydogan <erk.aydogan@ozu.edu.tr>
2016-12-24 01:43:34 +0200

    Designing Featured Events.

--- packages/Home/index.php
+++ packages/Home/index.php
@@ -7,16 +7,13 @@
 <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Raleway">
 <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
 <link href="../../includes/css/Home.css" rel="stylesheet">
+
 <?php
 include 'Database.php';
 $database = new Database();
 $list_catgs_query = "Select Category from Event group by Category";
 ?>
 
-<style>
-
-    body,h1,h2,h3,h4,h5,h6 {font-family: "Raleway", Arial, Helvetica, sans-serif}
-</style>
 <body>
 
 <!-- Navigation Bar -->
@@ -31,7 +28,7 @@ $list_catgs_query = "Select Category from Event group by Category";
 <div class="content" style="max-width: :1500px;">
 
     <div class="container matgin-top" id="Events">
-        <h3>Events</h3>
+        <h3>Search Your Event</h3>
     </div>
 
     <div class="w3-row-padding">
@@ -55,6 +52,43 @@ $list_catgs_query = "Select Category from Event group by Category";
             <button class="w3-btn-block">Search</button>
         </div>
     </div>
+    <h2>Featured Events</h2>
+
+
+    <div class="content padding" >
+
+        <div class="w3-row-padding">
+            <div class="w3-col l3 m6 w3-margin-bottom">
+                <div class="w3-display-container">
+                    <div class="w3-display-topleft w3-black w3-padding"> Istanbul Concert</div>
+                    <img src="../img/Concert.jpg" alt="Concert" style="width:100%">
+                </div>
+            </div>
+            <div class="w3-col l3 m6 w3-margin-bottom">
+                <div class="w3-display-container">
+                    <div class="w3-display-topleft w3-black w3-padding">Arsenal-Chelsea</div>
+                    <img src="../img/Sport.jpg" alt="Sport" style="width:100%">
+                </div>
+            </div>
+            <div class="w3-col l3 m6 w3-margin-bottom">
+                <div class="w3-display-container">
+                    <div class="w3-display-topleft w3-black w3-padding">International Festival</div>
+                    <img src="../img/Music.jpg" alt="Music" style="width:100%">
+                </div>
+            </div>
+            <div class="w3-col l3 m6 w3-margin-bottom">
+                <div class="w3-display-container">
+                    <div class="w3-display-topleft w3-black w3-padding">Modern Theater</div>
+                    <img src="../img/Theater.jpg" alt="Theater" style="width:100%"">
+                </div>
+            </div>
+
+        </div>
+
+
+
+        <div class="footer">Havefun.com &copy; 2016</div>
+
 
     <div class="mainselection">
         <select name="State" id="inputs">
@@ -66,7 +100,10 @@ $list_catgs_query = "Select Category from Event group by Category";
         </select>
     </div>
 
-    <div class="footer">Havefun.com &copy; 2016</div>
+
+
+
+
 </body>
 
 </html>
