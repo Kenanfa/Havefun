947ed253f5bd9298f90a14eb0b60699cc6b260dc
ErkAydogan <erk.aydogan@ozu.edu.tr>
2016-12-27 16:43:21 +0200

    Relation Between The Pages Developed.

--- packages/User/AdminProfile.php
+++ packages/User/AdminProfile.php
@@ -8,9 +8,8 @@
 <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
 
 <ul class="w3-navbar w3-white w3-large">
-    <li><a href="../Home/index.php" class="w3-black"></i>HaveFun</a></li>
-    <li class=><a href="../About/About.php">About</a></li>
-    <li><a href="../Contact/contact.php">Contact us</a></li>
+    <li><a href="../Profile/AdminHomePage.php" class="w3-black"></i>HaveFun</a></li>
+    <li class=><a href="../User/AdminProfile.php">Profile</a></li>
     <li class="w3-right w3-light-grey"><a href="../Home/index.php">Sign Out</a></li>
     <li class="w3-right w3-light-grey"><a href="../Event/CreateEvent.php">Create An Event</a></li>
 
--- packages/User/UserProfile.php
+++ packages/User/UserProfile.php
@@ -6,15 +6,15 @@
 <link rel="stylesheet" href="http://www.w3schools.com/lib/w3.css">
 <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Raleway">
 <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+<link href="../../includes/css/userprofile.css" rel="stylesheet">
 
 <ul class="w3-navbar w3-white w3-large">
-    <li><a href="../Home/index.php" class="w3-black"></i>HaveFun</a></li>
-    <li class=><a href="../About/About.php">About</a></li>
-    <li><a href="../Contact/contact.php">Contact us</a></li>
+    <li><a href="../Profile/UserHome.php" class="w3-black"></i>HaveFun</a></li>
+    <li class=><a href="../User/UserProfile.php">Profile</a></li>
     <li class="w3-right w3-light-grey"><a href="../Home/index.php">Sign Out</a></li>
 
 </ul>
-<link href="../../includes/css/userprofile.css" rel="stylesheet">
+
 
 
 
