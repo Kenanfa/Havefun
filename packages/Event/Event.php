fef439a1f5397096b1e56b8fd5c50a01e3a95039
ErkAydogan <erk.aydogan@ozu.edu.tr>
2016-12-27 21:22:53 +0200

    Event Page Design.

--- packages/Event/Event.php
+++ packages/Event/Event.php
@@ -5,7 +5,7 @@
 <meta name="viewport" content="width=device-width, initial-scale=1">
 <link rel="stylesheet" href="http://www.w3schools.com/lib/w3.css">
 <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Raleway">
-<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+<meta name="viewport" content="width=device-width, initial-scale=1">
 <link href="../../includes/css/event.css" rel="stylesheet">
 <ul class="w3-navbar w3-white w3-large">
     <li><a href="../Home/index.php" class="w3-black"></i>HaveFun</a></li>
@@ -16,6 +16,21 @@
 
 
 
+<h2>Event Information</h2>
 
+    <div class="login-page">
+        <div class="form">
+
+            <form class="Event-form" method="POST" action="../Login/SignIn.php">
+                <img src="../img/Sport.jpg" alt="Sport" height="132" width="440">
+                <h4>Event ID:</h4>
+                <h4>Event Name: <h4/>
+                    <h4>Event Category: <h4/>
+
+                        <button class="btn" type="submit" >Buy Tickets</button>
+                <p class="message"><a href="../Login/SignUp.php">Not Registered? Create An Account.</a></p>
+            </form>
+        </div>
+    </div>
 
 <div class="footer">Havefun.com &copy; 2016</div>
--- packages/Home/index.php
+++ packages/Home/index.php
@@ -47,7 +47,7 @@ include '../Header.php';
             <div class="w3-col l3 m6 w3-margin-bottom">
                 <div class="w3-display-container">
                 2    <div class="w3-display-topleft w3-black w3-padding"> <?php echo $row["Name"]; ?></div>
-                    <a href="../Event/Event.php">
+                    <a href="../packages/Event/Event.php">
                     <img src=<?php echo $row["Picture"]; ?> alt=<?php echo $row["Category"]; ?> style="width:100%">
                 </div>
             </div>
