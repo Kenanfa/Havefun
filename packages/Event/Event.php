cb944d0091516da57b8adceac0fa6618898fd681
ErkAydogan <erk.aydogan@ozu.edu.tr>
2016-12-27 14:49:23 +0200

    Create Event Design.

--- packages/Event/Event.php
+++ packages/Event/Event.php
@@ -1,9 +1,22 @@
-<?php include '../Header.php';?>
+<!DOCTYPE html>
+<html>
+<title>Havefun</title>
+<meta charset="UTF-8">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<link rel="stylesheet" href="http://www.w3schools.com/lib/w3.css">
+<link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Raleway">
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+<link href="../../includes/css/event.css" rel="stylesheet">
+<ul class="w3-navbar w3-white w3-large">
+    <li><a href="../Home/index.php" class="w3-black"></i>HaveFun</a></li>
+    <li class=><a href="../About/About.php">About</a></li>
+    <li><a href="../Contact/contact.php">Contact us</a></li>
+    <li class="w3-right w3-light-grey"><a href="../Login/SignIn.php">Sign in</a></li>
+
+</ul>
 
 
 
 
 
-
-
-<?php include '../Footer.php';?>
+<div class="footer">Havefun.com &copy; 2016</div>
--- packages/User/UserProfile.php
+++ packages/User/UserProfile.php
@@ -1,6 +1,20 @@
-<?php include '../Header.php';
-?>
+<!DOCTYPE html>
+<html>
+<title>Havefun</title>
+<meta charset="UTF-8">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<link rel="stylesheet" href="http://www.w3schools.com/lib/w3.css">
+<link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Raleway">
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
 
+<ul class="w3-navbar w3-white w3-large">
+    <li><a href="../Home/index.php" class="w3-black"></i>HaveFun</a></li>
+    <li class=><a href="../About/About.php">About</a></li>
+    <li><a href="../Contact/contact.php">Contact us</a></li>
+    <li class="w3-right w3-light-grey"><a href="../Home/index.php">Sign Out</a></li>
+
+</ul>
+<link href="../../includes/css/userprofile.css" rel="stylesheet">
 
 
 
