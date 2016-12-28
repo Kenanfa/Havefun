9cca995896268826106d2d5cde09960ea66a29cb
Kenanfa <kenan.fayoumi@ozu.edu.tr>
2016-12-28 22:08:58 +0300

    havefun.sql

--- Database/havefun.sql
+++ Database/havefun.sql
@@ -1,20 +1,6 @@
--- phpMyAdmin SQL Dump
--- version 4.6.4
--- https://www.phpmyadmin.net/
---
--- Host: 127.0.0.1
--- Generation Time: Dec 28, 2016 at 07:06 PM
--- Server version: 5.7.14
--- PHP Version: 5.6.25
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET time_zone = "+00:00";
-
-
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8mb4 */;
 
 --
 -- Database: `havefun`
