66446faadbdfa4ec2df4d9e8536de3b5d83578bd
ShakhKabi <shakhkabi@gmail.com>
2016-12-29 03:49:19 +0300

    header slight design

--- packages/Header.php
+++ packages/Header.php
@@ -1,7 +1,7 @@
 <!DOCTYPE html>
 <html>
 <link href="https://fonts.googleapis.com/css?family=Lobster" rel="stylesheet" type="text/css">
-<link href="../../includes/css/header.css" rel="stylesheet">
+
 
 <title>Havefun</title>
 <meta charset="UTF-8">
@@ -11,9 +11,9 @@
 <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
 
 <ul class="w3-navbar w3-white w3-large">
-    <li class="shrift"><a href="index.php" class="w3-black"></i>HaveFun</a></li>
-    <li class="shrift"><a href="About.php">About</a></li>
-    <li class="shrift"><a href="contact.php">Contact us</a></li>
-    <li class="w3-right w3-light-grey shrift"><a href="SignIn.php">Sign in</a></li>
+    <li style="font-family:Lobster; font-size:20px;"><a href="index.php" class="w3-black"></i>HaveFun</a></li>
+    <li style="font-family:Lobster; font-size:20px;"><a href="About.php">About</a></li>
+    <li style="font-family:Lobster; font-size:20px;"><a href="contact.php">Contact us</a></li>
+    <li class="w3-right w3-light-grey" style="font-family:Lobster; font-size:20px;"><a href="SignIn.php">Sign in</a></li>
 
 </ul>
