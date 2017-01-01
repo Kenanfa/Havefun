8143d943df0396722ca07014e9f2382f00d8c9c3
ShakhKabi <shakhkabi@gmail.com>
2017-01-02 02:46:18 +0300

    home page design

--- packages/index.php
+++ packages/index.php
@@ -8,40 +8,35 @@ include 'Header.php';
 <link href="../includes/css/Home.css" rel="stylesheet">
 <link href="https://fonts.googleapis.com/css?family=Lobster" rel="stylesheet" type="text/css">
 <body>
-<div>
-    <div class="container matgin-top" id="Events">
-        <h3 align="center" style="font-family: Lobster; font-size: 25px;" >Search Your Event</h3>
-    </div>
 
-    <form class="w3-row-padding" method="post" action="SearchIndex.php">
+    <header class="w3-display-container w3-content  w3-padding-64" style="max-width:1500px">
+           <div class="w3-container w3-padding-4 ">
+                <h3 align="center" style="font-family: Lobster; font-size:50px;">Search Your Event</h3>
 
-            <!--  Bro do what you want here but just make sure that
-             you put the date boxes in order:  Date FROM  --- Date TO  -->
-
-        <div class="w3-col m3">
-            <input class="EventDate2"  name = "from" type="date" placeholder="Search from">
-            <input class="EventDate"  name = "to" type="date" placeholder="To">
-            <input class="City" name = "city"type="text" placeholder="City">
-            <input class="Place" name = "place" type="text" placeholder="Place">
-            <button  type="submit" class="w3-btn-block">Search</button></form>
-                <select class="dropdown" name="category" id="inputs">
-                    <option value="All">All Categories</option>
-                    <?php $result = $database->performQuery($list_catgs_query);
-                    while($row = $result->fetch_assoc()) { ?>
-                        <option value=<?php echo $row["Category"]; ?>> <?php echo $row["Category"]; ?></option>
-                    <?php } ?>
-                </select>
+        <form class="w3-row-padding" method="post" action="SearchIndex.php">
+            <div class="w3-row-padding" align="center">
+                <div class="w3-rest">
+                    <input class="inputstyle" name = "from" type="date" placeholder="Search from">
+                    <input class="inputstyle" name = "to" type="date" placeholder="To">
+                    <input class="inputstyle" name = "city"type="text" placeholder="City">
+                    <input class="inputstyle" name = "place" type="text" placeholder="Place">
+                    <select  class="w3-dropdown-hover inputstyle" name="category"  id="inputs">
+                        <option  value="All">All Categories</option>
+                        <?php $result = $database->performQuery($list_catgs_query);
+                        while($row = $result->fetch_assoc()) { ?>
+                            <option value=<?php echo $row["Category"]; ?>> <?php echo $row["Category"]; ?></option>
+                        <?php } ?>
+                    </select>
+                </div>
+                   <p align="center" style="font-family:Lobster; font-size:18px;"><button  class="w3-btn w3-dark-black" type="submit" style="padding: 14px 40px">Search for event</button></p>
             </div>
-    </form>
-
-
-    <hr>
+        </form>
+</div>
+    </header>
     <h2 style="font-family: Lobster; font-size: 45px; ">Featured Events</h2>
-
-
-    <div class="content padding" >
-<form  class="login-form" method="POST" action="Event.php">
-        <div class="first">
+   <div class="container" style="padding-top: 180px">
+    <form  class="login-form" method="POST" action="Event.php">
+        <div>
             <?php $row = $randomEvents->fetch_assoc(); ?>
             <div class="w3-col l3 m6 w3-margin-bottom">
                 <div class="w3-display-container">
@@ -71,11 +66,8 @@ include 'Header.php';
                     <input type="image" class="imager" src=<?php echo $row["Picture"]; ?>  name="eventID" value=<?php echo $row["ID"]; ?> style="width:100%"  alt="Submit" />
                 </div>
             </div>
-
-        </div>
-
-    <div class="footer">Havefun.com &copy; 2016</div>
+        </form>
+   </div>
 </body>
-
 </html>
 
